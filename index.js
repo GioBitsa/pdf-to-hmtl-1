a1e76181ae1a1573d8f77da970598ec4c35856e9
Giorgi Bitsadze <bitsa@Giorgis-MacBook-Pro.local>
2024-07-23 00:04:10 +0400

    .

--- index.js
+++ index.js
@@ -24,3 +24,50 @@ rightContentClose.addEventListener("click", () => {
 
 const filterHeaderToggle = document.getElementById("bs-filterHeaderToggle");
 const filterHeader = document.getElementById("bs-filterHeader");
+
+function toggleDropdown(contentId) {
+  const dropdownContent = document.getElementById(contentId);
+  dropdownContent.style.display =
+    dropdownContent.style.display === "block" ? "none" : "block";
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (
+    !event.target.matches(".bs-mainDropdown") &&
+    !event.target.closest(".bs-mainDropdown")
+  ) {
+    const dropdowns = document.getElementsByClassName(
+      "bs-mainDropdown-content"
+    );
+    for (let i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i];
+      if (openDropdown.style.display === "block") {
+        openDropdown.style.display = "none";
+      }
+    }
+  }
+};
+
+function toggleEditors(id) {
+  const current = document.getElementById(id);
+  const allEditors = document.querySelectorAll(".bs-academicEditors");
+
+  allEditors.forEach((editor) => {
+    if (editor === current) {
+      editor.classList.toggle("open");
+    } else {
+      editor.classList.remove("open");
+    }
+  });
+}
+
+// Close all editors if the user clicks outside of them
+window.onclick = function (event) {
+  const allEditors = document.querySelectorAll(".bs-academicEditors");
+  if (!event.target.closest(".bs-academicEditors")) {
+    allEditors.forEach((editor) => {
+      editor.classList.remove("open");
+    });
+  }
+};
